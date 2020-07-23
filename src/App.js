9dc61960e060f4719d89b43bb473b782b9a69f29
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-07-23 23:51:50 +0300

    feat: add markdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import marked from "marked";
+import ReactMarkDown from "react-markdown";
 
-function App() {
+import "./App.css";
+
+export const App = () => {
+  const [markDown, setMarkDown] = useState(`
+  # Live demo
+
+Changes are automatically rendered as you type.
+
+## Table of Contents
+
+* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
+* Renders actual, "native" React DOM elements
+* Allows you to escape or skip HTML (try toggling the checkboxes above)
+* If you escape or skip the HTML, no 'dangerouslySetInnerHTML' is used! Yay!
+
+## HTML block below
+
+<blockquote>
+  This blockquote will change based on the HTML settings above.
+</blockquote>
+
+But
+> This blockquote will change based on the HTML settings above.
+
+
+Pretty neat, eh?
+
+## Tables?
+
+| Feature   | Support |
+| --------- | ------- |
+| tables    | ✔ |
+| alignment | ✔ |
+| wewt      | ✔ |
+  `);
+
+  const onChange = (e) => {
+    setMarkDown(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <textarea onChange={onChange} value={markDown} />
+
+      {/* 1 way */}
+      {/* <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(markDown) }}
+      /> */}
+
+      {/* 2 way */}
+      <ReactMarkDown className="preview" source={markDown} />
     </div>
   );
-}
-
-export default App;
+};
